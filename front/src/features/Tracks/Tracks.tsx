668fda40a875e5473c6fb56526df205d83179c82
Maksim Tkashev <maks93makula@gmail.com>
2024-04-26 15:58:05 +0300

    Создан роут для треков у аристов, сделана верстка и типизации

---
+++ front/src/features/Tracks/Tracks.tsx
@@ -0,0 +1,64 @@
+import {useAppDispatch, useAppSelector} from '../../App/hooks.ts';
+import {selectIsLoading, selectTracks} from '../../store/tracksSlice.ts';
+import {useEffect} from 'react';
+import {getAlbums, getArtists, getTracks} from '../../store/asynсThunks.ts';
+import {useParams} from 'react-router-dom';
+import {Card, CardContent, CircularProgress, Grid, Typography} from '@mui/material';
+import {selectArtists} from '../../store/artistsSlice.ts';
+import {selectAlbums} from '../../store/albumsSlice.ts';
+
+const Tracks = () => {
+  const dispatch = useAppDispatch();
+  const tracks = useAppSelector(selectTracks);
+  const isLoading = useAppSelector(selectIsLoading);
+  const artistName = useAppSelector(selectArtists);
+  const albumName = useAppSelector(selectAlbums);
+
+  const params = useParams();
+
+  const album = albumName.find(elem => elem._id === params.id);
+  const artist = artistName.find(elem => elem._id === album?.artist);
+
+  useEffect(() => {
+    const fetchUrl = async () => {
+      if (params.id) {
+        await dispatch(getTracks(params.id));
+
+        if (artist) {
+          await dispatch(getAlbums(artist._id));
+          await dispatch(getArtists());
+        }
+      }
+    };
+
+    void fetchUrl();
+  }, [dispatch, params.id]);
+
+  return (
+    <>
+      {album && artist && <Typography variant="h4">Artist: {artist.name}, Album: {album.name}</Typography>}
+
+      <Grid container spacing={10} mt={3}>
+        {!isLoading ? tracks.map((elem) => (
+          <Grid item xs={3} key={elem._id}>
+            <Card sx={{ minWidth: 230 }}>
+              <CardContent>
+                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                  {'number: ' + elem.number}
+                </Typography>
+                <Typography variant="h5" component="div">
+                  {'track: ' + elem.name}
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                  {'duration: ' + elem.duration}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        )) : <CircularProgress />}
+      </Grid>
+    </>
+  );
+};
+
+export default Tracks;
